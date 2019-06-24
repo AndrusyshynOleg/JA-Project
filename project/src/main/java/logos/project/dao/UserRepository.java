b3805770ed3f4df84cf23d38a52a1f1d371be05b
Andrusyshyn Oleg <Andrusyshynoleg@gmail.com>
2019-06-24 11:40:43 +0300

    added method find by email

--- project/src/main/java/logos/project/dao/UserRepository.java
+++ project/src/main/java/logos/project/dao/UserRepository.java
@@ -1,9 +1,11 @@
 package logos.project.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import logos.project.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	List<User> findByEmail(String email);
 }
